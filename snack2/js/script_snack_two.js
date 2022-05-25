30c8f337f04d43923f9f012099987d52b36fa1be
Al-E-00 <mazzellaalessio6@gmail.com>
2022-05-25 15:19:06 +0200

    snack two problem!!

---
+++ snack2/js/script_snack_two.js
@@ -0,0 +1,21 @@
+
+/* 
+- Let enter a number (n) from the user
+- generates N arrays (made by 10 random numbers (from 1 to 100))
+- Print the result
+*/
+
+let userNumber = parseInt(prompt("Enter a number"));
+let arraysNumber = 0;
+
+while (arraysNumber <= userNumber){
+
+    let arrays = [];
+    while (arrays[] < 10) {
+        let casualNumber = (Math.floor(Math.random * 100));
+        arrays.push(casualNumber);
+    }
+
+    arraysNumber = arraysNumber + 1;
+}
+
