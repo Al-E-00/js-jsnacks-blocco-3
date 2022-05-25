fabeeec14f495e817e3f521832eadc50963bf0b0
Al-E-00 <mazzellaalessio6@gmail.com>
2022-05-25 15:52:09 +0200

    snack 3 -> done

--- snack 3/js/script_snack_three.js
+++ snack 3/js/script_snack_three.js
@@ -1,5 +1,20 @@
 
 /* 
-Ask 5 times to user to enter a number
-- it print the sum of all the numbers entered
+- Ask 5 times to user to enter a number
+- the program print the sum of all the entered numbers
 */
+
+
+let askedTimes = 0;
+let sum = 0;
+
+while (askedTimes < 5) {
+    let userNumber = parseInt(prompt("Enter a number"));
+    if (!isNaN(userNumber)){
+        sum += userNumber;
+
+        askedTimes += + 1;
+    }
+}
+
+console.log(sum);
